2ac3fb4784aa6915c0b22d7de9d8052b98080446
Francis Oteng <francisoteng1.05@gmail.com>
2025-10-14 10:37:32 +0000

    InitialCommit

---
+++ script.js
@@ -0,0 +1,6 @@
+        alert("Welcome to my website!");
+
+  let listContent= document.getElementById('list').innerHTML;
+  document.writeln(listContent);
+
+      document.getElementById('list').innerHTML;
